e550a38164b4393023222526f5a29b414d40df99
John Wang <johncwang@gmail.com>
2024-06-19 13:03:36 -0700

    feat: `cmd`: add `get_token`

---
+++ cmd/get_token/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/grokify/go-saviynt"
+	"github.com/grokify/mogo/config"
+	"github.com/grokify/mogo/fmt/fmtutil"
+	"github.com/grokify/mogo/log/logutil"
+)
+
+func main() {
+	_, err := config.LoadDotEnv([]string{".env"}, 1)
+	logutil.FatalErr(err)
+
+	tok, err := saviynt.GetToken(
+		os.Getenv("SAVIYNT_BASE_URL"),
+		os.Getenv("SAVIYNT_USERNAME"),
+		os.Getenv("SAVIYNT_PASSWORD"))
+	logutil.FatalErr(err)
+
+	fmtutil.MustPrintJSON(tok)
+
+	fmt.Println("DONE")
+}
