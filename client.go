d57febf139ef209dd54c16f1e9e19ef741b25061
John Wang <johncwang@gmail.com>
2024-11-10 05:38:14 -0800

    enhance: `lint`: update various

--- client.go
+++ client.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	RelURLLogin                    = "/ECM/api/login"
-	RelOAuthAccessToken            = "/ECM/oauth/access_token"
+	RelOAuthAccessToken            = "/ECM/oauth/access_token" // #nosec G101
 	RelURLECM                      = "/ECM"
 	RelURLAPI                      = "/api/v5"
 	RelURLLoginRuntimeControlsData = "/fetchRuntimeControlsDataV2" // API at https://documenter.getpostman.com/view/23973797/2s9XxwutWR#b821cc21-ee7c-49e3-9433-989ed87b2b03
