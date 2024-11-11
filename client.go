edd5503b287c875c461500adf18e1e2f50582a82
John Wang <johncwang@gmail.com>
2024-11-10 22:42:26 -0800

    refactor: `saviynt`: move contants to `constants.go`

--- client.go
+++ client.go
@@ -17,18 +17,6 @@ import (
 	"golang.org/x/oauth2"
 )
 
-const (
-	RelURLLogin                    = "/ECM/api/login"
-	RelOAuthAccessToken            = "/ECM/oauth/access_token" // #nosec G101
-	RelURLECM                      = "/ECM"
-	RelURLAPI                      = "/api/v5"
-	RelURLLoginRuntimeControlsData = "/fetchRuntimeControlsDataV2" // API at https://documenter.getpostman.com/view/23973797/2s9XxwutWR#b821cc21-ee7c-49e3-9433-989ed87b2b03
-
-	RelURLPasswordChange = "/changePassword"
-	RelURLUserGet        = "/getUser"
-	RelURLUserUpdate     = "/updateUser"
-)
-
 type Client struct {
 	BaseURL      string
 	Path         string
--- constants.go
+++ constants.go
@@ -1,6 +1,17 @@
 package saviynt
 
 const (
+	RelURLLogin         = "/ECM/api/login"
+	RelOAuthAccessToken = "/ECM/oauth/access_token" // #nosec G101
+	RelURLECM           = "/ECM"
+	RelURLAPI           = "/api/v5"
+
+	RelURLLoginRuntimeControlsData = "/fetchRuntimeControlsDataV2" // API at https://documenter.getpostman.com/view/23973797/2s9XxwutWR#b821cc21-ee7c-49e3-9433-989ed87b2b03
+	RelURLUserGetAccessDetails     = "/getAccessDetailsForUser"
+	RelURLPasswordChange           = "/changePassword"
+	RelURLUserGet                  = "/getUser"
+	RelURLUserUpdate               = "/updateUser"
+
 	EnvSaviyntServerURL = "SAVIYNT_SERVER_URL"
 	EnvSaviyntUsername  = "SAVIYNT_USERNAME"
 	EnvSaviyntPassword  = "SAVIYNT_PASSWORD"
