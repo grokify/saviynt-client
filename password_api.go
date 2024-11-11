f885e960c57d84bd0a5b64d4eda748c4968b31e2
John Wang <johncwang@gmail.com>
2024-11-10 22:43:00 -0800

    docs: `ChangePasswordOpts`: add field comments

--- password_api.go
+++ password_api.go
@@ -30,13 +30,38 @@ func (svc *PasswordService) ChangePassword(opts ChangePasswordOpts) (*http.Respo
 }
 
 type ChangePasswordOpts struct {
-	Username                         string   `json:"username"`
-	Password                         string   `json:"password"`
-	ChangePasswordAssociatedAccounts bool     `json:"changePasswordAssociatedAccounts"`
-	ChangeUserPassword               bool     `json:"changeUserPassword"`
-	Endpoints                        []string `json:"endpoints"`
-	SetARSTaskSource                 bool     `json:"setarstasksource"`
-	ValidateAgainstPolicy            bool     `json:"validateagainstpolicy"`
+	// Username specifies the name of the user for whom you need to change the password.
+	Username string `json:"username"`
+
+	// Password specifies the new password.
+	Password string `json:"password"`
+
+	// ChangePasswordAssociatedAccounts specifies whether to create the corresponding Change Password task, if `true`. You can specify `false` to only update the user password. The default value is `true`.
+	ChangePasswordAssociatedAccounts bool `json:"changePasswordAssociatedAccounts"`
+
+	// Endpointspecifies a list of endpoints (comma-separated) to update the password for
+	// if `ChangePasswordAssociatedAccounts` is set to `true`.
+	Endpoint []string `json:"endpoint"`
+
+	// SetARSTaskSourcespecifies whether to set the source column in the `arstasks`
+	// table with the `changeOwnPasswordFromAPI` value. When the source column in `arstasks`
+	// table is set to the `changeOwnPasswordFromAPI` value then the `pwdLastSet` parameter is
+	// not set to `0`` for Active Directory (AD). The default value is `false`.  Note: If your
+	// AD password has expired (pwdLastSet = 0), you are forced to choose a new password in EIC
+	// on the next login.
+	SetARSTaskSource bool `json:"setarstasksource"`
+
+	// UpdateUserPassword specifies whether to update the user password when
+	// `ChangePasswordAssociatedAccounts` is set to `true`. Setting this to `true` specifies
+	// when `ChangePasswordAssociatedAccounts` is `true` updates the user password and creates
+	// the corresponding Change Password task. When set to `false``, only the Change Password
+	// task is created. The default value is true.
+	UpdateUserPassword bool `json:"updateUserPassword"`
+
+	// ValidateAgainstPolicy specifies whether the new password conforms to the USER scope
+	// password policy. If you do not want to apply the password policy, then specify `N`.
+	// The default value is `Y`.
+	ValidateAgainstPolicy bool `json:"validateagainstpolicy"`
 }
 
 func (opts ChangePasswordOpts) Values() url.Values {
@@ -44,14 +69,14 @@ func (opts ChangePasswordOpts) Values() url.Values {
 	v.Add("username", opts.Username)
 	v.Add("password", opts.Password)
 	v.Add("changePasswordAssociatedAccounts", strconvutil.Btoa(opts.ChangePasswordAssociatedAccounts))
-	v.Add("changeUserPassword", strconvutil.Btoa(opts.ChangeUserPassword))
+	v.Add("updateUserPassword", strconvutil.Btoa(opts.UpdateUserPassword))
 	v.Add("setarstasksource", strconvutil.Btoa(opts.SetARSTaskSource))
 	if opts.ValidateAgainstPolicy {
 		v.Add("validateagainstpolicy", "Y")
 	} else {
 		v.Add("validateagainstpolicy", "N")
 	}
-	eps := stringsutil.SliceCondenseSpace(opts.Endpoints, true, false)
+	eps := stringsutil.SliceCondenseSpace(opts.Endpoint, true, false)
 	if len(eps) > 0 {
 		v.Add("endpoints", strings.Join(eps, ","))
 	}
